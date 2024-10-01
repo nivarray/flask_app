f78da97b11d6c3ade98d345795c4e565dba2138e
Raymond Nivar <nivarraymond@gmail.com>
2024-10-01 17:51:40 -0400

    Modified get_db() with a try except clause to handle DB connection issues

--- app/db.py
+++ app/db.py
@@ -7,10 +7,14 @@ from flask import g
 # g is a global request context, stores data temporarily
 # for the duration of the request. perfect for db connections.
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect('instance/app.db')
-        g.db.row_factory = sqlite3.Row  # ensures that results are returned as a dictionary-like object, where you can access columns by name.
-    return g.db
+    try:
+        if 'db' not in g:
+            g.db = sqlite3.connect('instance/app.db')
+            g.db.row_factory = sqlite3.Row  # ensures that results are returned as a dictionary-like object, where you can access columns by name.
+        return g.db
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return None
 
 #prevents potential resource leaks
 def close_db(e=None):
